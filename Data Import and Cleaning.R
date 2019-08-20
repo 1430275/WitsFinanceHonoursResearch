b31a1c2835449abe9eaafff97ea3398cfa99cb92
Shane <6067105+Shanes927@users.noreply.github.com>
2019-08-20 19:01:08 +0200

    Add Drive Data import

--- Data Import and Cleaning.R
+++ Data Import and Cleaning.R
@@ -1,16 +1,37 @@
-rm(list=ls())
+rm(list = ls())
 
-
-library(readxl)
+library(googledrive)
+library(readr)
 library(janitor)
-PriceData <- read_excel("C:/Users/shane/University/2019/Research Report/Data/Price-Volume-MarketCap.xlsx", 
-                        sheet = "Price (D)", 
-                        na = ""
-                        )
-
-typeof(PriceData)
-df <-  as.data.frame(PriceData)
 
 
+tempdirectory <- tempdir()
 
-df1 <-  remove_empty(df, "cols")
+tempP <- tempfile(tmpdir = tempdirectory, fileext = ".zip")
+dlP <- drive_download(as_id("1FWCX68QO9VUT5HVPP2rMuA5GI5XH2JVl"), 
+                      path = tempP, 
+                      overwrite = TRUE)
+outP <- unzip(tempP, exdir = tempdirectory)
+PriceData <- read.csv(outP[1], sep=",")
+
+tempV <- tempfile(tmpdir = tempdirectory, fileext = ".zip")
+dlV <- drive_download(as_id("1tfeAcTXbJT-Md-Wsae0LfLJJ8M7k7uw5"),
+                      path = tempV,
+                      overwrite = TRUE)
+outV <- unzip(tempV, exdir = tempdirectory)
+VolumeData <- read.csv(outV[1], sep=",")
+
+tempM <- tempfile(tmpdir = tempdirectory, fileext = ".zip")
+dlM <- drive_download(as_id("1SxQtzVoAN8HczZLI8LZzsWDYmYDItdl8"), 
+                      path = tempM,
+                      overwrite = TRUE)
+outM <- unzip(tempM, exdir = tempdirectory )
+MCapData<- read.csv(outM[1], sep=",")
+
+PricedfTemp <-  as.data.frame(PriceData)
+VolumedfTemp <-  as.data.frame(VolumeData)
+MCapdfTemp <-  as.data.frame(MCapData)
+
+Pricedf <-  remove_empty(PricedfTemp, "cols")
+Volumedf <-  remove_empty(VolumedfTemp, "cols")
+MCapdf <-  remove_empty(MCapdfTemp, "cols")
