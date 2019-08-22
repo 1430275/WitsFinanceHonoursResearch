b9936b2d95a1b289e9ac718969b76b54c47fc846
Shane <6067105+Shanes927@users.noreply.github.com>
2019-08-22 20:30:34 +0200

    Return data calculated

--- Data Import and Cleaning.R
+++ Data Import and Cleaning.R
@@ -3,19 +3,26 @@ rm(list = ls())
 library(googledrive)
 library(readxl)
 library(janitor)
+library(naniar)
+library(xts)
+library(lubridate)
 
 id <-  "1SGigXMnzubpP15Y1W18GqnlVQFiv95jN"
 try(drive_download(as_id(id), overwrite = FALSE), silent = TRUE)
 
 PriceData <- read_excel("Price-Volume-MarketCap.xlsx", 
                         sheet = "Price (D)") 
+PriceData <- replace_with_na_all(PriceData, ~.x < 100)
 VolumeData <- read_excel("Price-Volume-MarketCap.xlsx", 
                         sheet = "Volume (D)") 
-MCapData <- read_excel("Price-Volume-MarketCap.xlsx", 
-                        sheet = "Mcap (D)") 
+VolumeData <- replace_with_na_all(VolumeData, ~.x < 1)
 
 Pricedf <-  remove_empty(PriceData, "cols")
 Volumedf <-  remove_empty(VolumeData, "cols")
-MCapdf <-  remove_empty(MCapData, "cols")
 
-rm(PriceData, VolumeData, MCapData, id)
+rm(PriceData, VolumeData, id)
+
+Pricedf$Date <-  as.Date(Pricedf$Date)
+
+Returns <- xts(Pricedf[,-1], order.by= Pricedf$Date)
+Returns <- diff(Returns, arithmetic=FALSE ) - 1
