808b53ee34a0771f10019da24d98755ef33e2781
Shane <6067105+Shanes927@users.noreply.github.com>
2019-08-23 12:05:04 +0200

    Max drawdown done with fix to N/A removal

--- Data Import and Cleaning.R
+++ Data Import and Cleaning.R
@@ -7,22 +7,49 @@ library(naniar)
 library(xts)
 library(lubridate)
 
+#settings
+lkbk <- 5 
+
 id <-  "1SGigXMnzubpP15Y1W18GqnlVQFiv95jN"
 try(drive_download(as_id(id), overwrite = FALSE), silent = TRUE)
 
 PriceData <- read_excel("Price-Volume-MarketCap.xlsx", 
                         sheet = "Price (D)") 
-PriceData <- replace_with_na_all(PriceData, ~.x < 100)
 VolumeData <- read_excel("Price-Volume-MarketCap.xlsx", 
                         sheet = "Volume (D)") 
-VolumeData <- replace_with_na_all(VolumeData, ~.x < 1)
 
-Pricedf <-  remove_empty(PriceData, "cols")
-Volumedf <-  remove_empty(VolumeData, "cols")
+NaFunction <-  function(column){
+  !all(is.na(column))
+}
 
-rm(PriceData, VolumeData, id)
+exclP <-  sapply(PriceData, NaFunction)
+exclV <-  sapply(VolumeData, NaFunction)
 
+Pricedf <-  PriceData[exclP]
 Pricedf$Date <-  as.Date(Pricedf$Date)
+Volumedf <-  VolumeData[exclV]
+#Volumedf$Date <-  as.Date(Volumedf$Date)
 
-Returns <- xts(Pricedf[,-1], order.by= Pricedf$Date)
-Returns <- diff(Returns, arithmetic=FALSE ) - 1
+rm(PriceData, VolumeData, id, exclP, exclV)
+
+maxDD <-  function(column, lb){
+  dd <- vector(mode = "double", length = (length(column) - lb))
+  for (i in (lb+1):length(column)){
+    if(is.na(max(column[(i-lb):i]))){
+      dd[i] <- NA
+    } else{
+      dd[i] <- (column[i] - max(column[(i-lb):i], na.rm = T))/max(column[(i-lb):i], na.rm = T)
+    }
+  }
+  return(dd)
+}
+
+ddDf <-  as.data.frame(sapply(Pricedf[-1], maxDD, lb = lkbk))
+
+d <- Pricedf[[1]]
+
+d[ddDf[[1]] < -0.2]
+
+
+test <- c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)
+maxDD(test, lkbk)
