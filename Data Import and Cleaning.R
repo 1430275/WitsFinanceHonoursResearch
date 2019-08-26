b28f5b0cb400d72d5e1d7aa5b61a8c79820de415
Shane <6067105+Shanes927@users.noreply.github.com>
2019-08-26 17:15:12 +0200

    Formatting Changes

--- Data Import and Cleaning.R
+++ Data Import and Cleaning.R
@@ -18,24 +18,25 @@ VolumeData <- read_excel("Price-Volume-MarketCap.xlsx",
 
 #file.remove("Price-Volume-MarketCap.xlsx")
 
+
+###Function Declaration
 NaFunction <-  function(column){
   !all(is.na(column))
 }
 
-#ZAr100Filter <-  function(column){
-#  PriceData<- vector(mode = "double", length = (length(column)))
-#  for (i in 1:length(column)){
-#    if(column[i] < 100){
-#      PriceData[i] <- NA
-#    } else{
-#      PriceData[i]
-#    }
-#  }
-#  return(PriceData)
-#}
+ZAr100Filter <-  function(column){
+  PriceData<- vector(mode = "double", length = (length(column)))
+  for (i in 1:length(column)){
+    if(column[i] < 100){
+      PriceData[i] <- NA
+    } else{
+      PriceData[i]
+    }
+  }
+  return(PriceData)
+}
 
 #test <- as.data.frame(sapply(PriceData[-1], ZAr100Filter))
-
 
 exclP <-  sapply(PriceData, NaFunction)
 exclV <-  sapply(VolumeData, NaFunction)
@@ -43,8 +44,7 @@ exclV <-  sapply(VolumeData, NaFunction)
 Pricedf <-  PriceData[exclP]
 Pricedf$Date <-  as.Date(Pricedf$Date)
 Volumedf <-  VolumeData[exclV]
-#Volumedf$Date <-  as.Date(Volumedf$Date)
-
+Volumedf$Date <-  as.Date(Volumedf$Date)
 
 maxDD <-  function(column, lb){
   dd <- vector(mode = "double", length = (length(column) - lb))
@@ -58,10 +58,6 @@ maxDD <-  function(column, lb){
   return(dd)
 }
 
-DDdf <-  as.data.frame(sapply(Pricedf[-1], maxDD, lb = lkbk))
-DDdf <-  cbind(Pricedf$Date, DDdf)
-names(DDdf)[names(DDdf) == "Pricedf$Date"] <- "Date"
-
 minDU <-  function(column, lb){
   du <- vector(mode = "double", length = (length(column) - lb))
   for (i in (lb+1):length(column)){
@@ -74,14 +70,18 @@ minDU <-  function(column, lb){
  return(du)
 }
 
+DDdf <-  as.data.frame(sapply(Pricedf[-1], maxDD, lb = lkbk))
+DDdf <-  cbind(Pricedf$Date, DDdf)
+names(DDdf)[names(DDdf) == "Pricedf$Date"] <- "Date"
+
 DUdf <-  as.data.frame(sapply(Pricedf[-1], minDU, lb = lkbk))
 DUdf <-  cbind(Pricedf$Date, DUdf)
 names(DUdf)[names(DUdf) == "Pricedf$Date"] <- "Date"
-
 
 dateDD <- DDdf$Date
 dateDU <- DUdf$Date
 
 DDtrig <- lapply(2:ncol(DDdf), function(i) dateDD[DDdf[[i]] < - 0.15])
+DUtrig <- lapply(2:ncol(DUdf), function(i) dateDU[DUdf[[i]] < - 0.15])
 
-rm(list=setdiff(ls(), "DDtrig"))
+rm(list=setdiff(ls(), c("DDtrig", "DUtrig")))
