2220efaabb7961cd45d34f348428774b970f892f
Shane <6067105+Shanes927@users.noreply.github.com>
2019-08-28 22:42:12 +0200

    Update Main Script.R

--- Main Script.R
+++ Main Script.R
@@ -1,16 +1,9 @@
 rm(list = ls())
 
-###################################
-#Please see line 86 and beyond for reprex data and functions we need help with
-###################################
-
 library(googledrive)
 library(readxl)
 library(xts)
 library(lubridate)
-
-#settings
-lkbk <- 5 
 
 id <-  "1SGigXMnzubpP15Y1W18GqnlVQFiv95jN"
 try(drive_download(as_id(id), overwrite = FALSE), silent = TRUE)
@@ -50,6 +43,13 @@ PriceData$Date <-  as.Date(PriceData$Date)
 VolumeData <-  VolumeData[exclV]
 VolumeData$Date <-  as.Date(VolumeData$Date)
 
+
+#settings
+lookback <- 5 
+triggerDD <- -0.15
+triggerDU <- 0.15
+window <- 5
+
 maxDD <-  function(column, lb){
   dd <- vector(mode = "double", length = (length(column) - lb))
   for (i in (lb+1):length(column)){
@@ -61,6 +61,10 @@ maxDD <-  function(column, lb){
   }
   return(dd)
 }
+
+DDdf <-  as.data.frame(sapply(PriceData[-1], maxDD, lb = lookback))
+DDdf <-  cbind(PriceData$Date, DDdf)
+names(DDdf)[names(DDdf) == "PriceData$Date"] <- "Date"
 
 minDU <-  function(column, lb){
   du <- vector(mode = "double", length = (length(column) - lb))
@@ -74,131 +78,50 @@ minDU <-  function(column, lb){
  return(du)
 }
 
-DDdf <-  as.data.frame(sapply(PriceData[-1], maxDD, lb = lkbk))
-DDdf <-  cbind(PriceData$Date, DDdf)
-names(DDdf)[names(DDdf) == "PriceData$Date"] <- "Date"
-
-DUdf <-  as.data.frame(sapply(PriceData[-1], minDU, lb = lkbk))
+DUdf <-  as.data.frame(sapply(PriceData[-1], minDU, lb = lookback))
 DUdf <-  cbind(PriceData$Date, DUdf)
 names(DUdf)[names(DUdf) == "PriceData$Date"] <- "Date"
 
-
-####Reprex Data
-
-datesEg <- seq(as.Date("2014-09-04"), by = "day", length.out = 6)
-#DDsub <-DDdf[c(1:15),c(2:6)]
-#DUsub <-DUdf[c(1:15),c(2:6)]
-
-DDsub <-  data.frame("v1" = c(0, -0.0012, -0.1612, -0.1953, -0.2432, -0.1122), "v2" = c(0, 0, 0, 0, 0, 0), "v3" = c(0, -0.0021, -0.1233, -0.1242, -0.0043, -0.0033),"v4" = c(0, -0.0031, -0.3094, -0.1023, -0.0984, -0.01235),"v5" = c(0, NA, NA, NA, NA, NA))
-
-DUsub <-  abs(DDsub)
-reprexDD <- data.frame(datesEg, DDsub)  
-reprexDU <- data.frame(datesEg, DUsub) 
-
-##################################################
-#We're Trying to get this to work
-###This pulls dates and forms a list of lists of dates that fall under the 15% trigger.  
-
-#dateDD <- DDdf$Date
-#dateDU <- DUdf$Date
-
-#DDtrig <- lapply(2:ncol(DDdf), function(i) dateDD[DDdf[[i]] < - 0.15])
-#DUtrig <- lapply(2:ncol(DUdf), function(i) dateDU[DUdf[[i]]  > 0.15])
-
-
-#This removes all NA columns - (may not be necessary)
-
-#exclDD <-  sapply(DDtrig, NaFunction)
-#DDtrig <-  DDtrig[exclDD]
-
-#exclDU <-  sapply(DUtrig, NaFunction)
-#DUtrig <-  DUtrig[exclDU]
-
-##################################################
-
-MM
-
-####Reprex Data
-
-rm(list = ls())
-
-datesEg <- seq(as.Date("2014-09-04"), by = "day", length.out = 6)
-#DDsub <-DDdf[c(1:15),c(2:6)]
-#DUsub <-DUdf[c(1:15),c(2:6)]
-
-DDsub <-  data.frame("v1" = c(0, -0.0012, -0.1612, -0.1953, -0.2432, -0.1122),
-                     "v2" = c(0, 0, 0, 0, 0, 0),
-                     "v3" = c(0, -0.0021, -0.1233, -0.1242, -0.0043, -0.0033),
-                     "v4" = c(0, -0.0031, -0.3094, -0.1023, -0.0984, -0.01235),
-                     "v5" = c(0, NA, NA, NA, NA, NA))
-
-DUsub <-  abs(DDsub)
-reprexDD <- data.frame(datesEg, DDsub)  
-reprexDU <- data.frame(datesEg, DUsub) 
-
-# Your code below doesn't correspond to the objects you created.
-# But I get the question so I will work with some assumptions:
-# Because you only have 6 days of data, I will say you need to ignore dates that occur in the following two days and the trigger value is 10%.
-# This means means our dates should be: 
-# v1: 2014-09-06, 2014-09-09
-# v2: blank
-# v3: 2014-09-06
-# v4: 2014-09-06
-# v5: blank
-
-# Let's start by setting parameters:
-
-trigger <- 0.1
-window <- 2
-
-# Now we need to go top to bottom and start by getting our first value below the trigger.
-# We will be using the relatively uncommon "while" loop
-# Be careful with while loops because if you make a mistake they can run forever.
-# Our first loop will just provide the positions.
-
-solution <- vector(mode = "integer", length = 1)
-
-l = length(reprexDD$v1)
-s = 0
-pos = 1
-while(s<=l){
-  solution[pos] = which(reprexDD$v1[(s+1):l] <= -trigger)[1]+s
-  s = solution[pos] + window
-  pos = pos + 1
-}
-
-# Now we can return the dates of those positions:
-
-datesEg[solution]
-
-# Finally we can remove the naming of the variables and get the loop to run on each column.
-
-trigger_index <- lapply(reprexDD[-1], function(i){
-  solution <- vector(mode = "integer", length = 1)
+trigIndexDD <- lapply(DDdf[-1], function(i){
+  trigDD <- vector(mode = "integer", length = 1)
   l = length(i)
   s = 0
   pos = 1
   while(s <= l){
-    x = which(i[(s+1):l] <= -trigger)[1]+s
+    x = which(i[(s+1):l] <= -triggerDD)[1]+s
     if(is.na(x)){
       break
     } else{
-      solution[pos] = x
+      trigDD[pos] = x
     }
-    s = solution[pos] + window
+    s = trigDD[pos] + window
     pos = pos + 1
   }
-  return(solution)
+  return(trigDD)
 })
 
-
-# This looks good and it works the way we would expect.
-# We can now simply pass the index values for each variable through the date vector:
-
-lapply(trigger_index, function(z){
-  datesEg[z]
+lapply(trigIndexDD, function(x){
+  DDdf$Date[x]
 })
 
-# Or use a for loop or whatever makes more sense to you.
+trigIndexDU <- lapply(DUdf[-1], function(i){
+  trigDU <- vector(mode = "integer", length = 1)
+  l = length(i)
+  s = 0
+  pos = 1
+  while(s <= l){
+    x = which(i[(s+1):l] <= -triggerDU)[1]+s
+    if(is.na(x)){
+      break
+    } else{
+      trigDU[pos] = x
+    }
+    s = trigDU[pos] + window
+    pos = pos + 1
+  }
+  return(trigDU)
+})
 
-# You can then drop any vectors that have a length of zero?
+lapply(trigIndexDU, function(x){
+  DUdf$Date[x]
+})
